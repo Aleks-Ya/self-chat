5bc835fb74aea50cb192bfd18a77b7d6a8f8be71
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2016-06-25 11:11:52 +0300

    add vaadin HelloWorld

---
+++ src/main/java/ru/yaal/seflchat/HelloWorldUI.java
@@ -0,0 +1,13 @@
+package ru.yaal.seflchat;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+
+@SuppressWarnings("WeakerAccess")
+public class HelloWorldUI extends UI {
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        setContent(new Label("Hello World!"));
+    }
+}
---
+++ src/main/java/ru/yaal/seflchat/UIServlet.java
@@ -0,0 +1,11 @@
+package ru.yaal.seflchat;
+
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinServlet;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(urlPatterns = "/*", name = "UIServlet", asyncSupported = true)
+@VaadinServletConfiguration(ui = HelloWorldUI.class, productionMode = false)
+public class UIServlet extends VaadinServlet {
+}
