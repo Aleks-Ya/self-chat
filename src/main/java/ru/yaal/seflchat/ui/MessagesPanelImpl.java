fa1a74e6e4365de55a8503c1257e0fa6b40723e5
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2016-06-27 08:10:00 +0300

    scroll MessagesPanel

--- src/main/java/ru/yaal/seflchat/ui/MessagesPanelImpl.java
+++ src/main/java/ru/yaal/seflchat/ui/MessagesPanelImpl.java
@@ -3,17 +3,21 @@ package ru.yaal.seflchat.ui;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
 import ru.yaal.seflchat.data.Message;
 
 /**
  * @author Yablokov Aleksey
  */
-public class MessagesPanelImpl extends VerticalLayout implements MessagesPanel {
+public class MessagesPanelImpl extends Panel implements MessagesPanel {
     private boolean nextMessageRightAlignment = false;
+    private final VerticalLayout vertical = new VerticalLayout();
 
     public MessagesPanelImpl() {
-        setSpacing(true);
+        setHeight(70, Unit.PERCENTAGE);
+        vertical.setSpacing(true);
+        setContent(vertical);
     }
 
     @Override
@@ -22,13 +26,13 @@ public class MessagesPanelImpl extends VerticalLayout implements MessagesPanel {
         label.setContentMode(ContentMode.HTML);
         label.setReadOnly(true);
         label.setWidth(70, Unit.PERCENTAGE);
-        addComponent(label);
+        vertical.addComponent(label);
         if (nextMessageRightAlignment) {
             label.setValue("<p align='right'>" + message.getContent() + "</p>");
-            setComponentAlignment(label, Alignment.TOP_RIGHT);
+            vertical.setComponentAlignment(label, Alignment.TOP_RIGHT);
         } else {
             label.setValue("<p align='left'>" + message.getContent() + "</p>");
-            setComponentAlignment(label, Alignment.TOP_LEFT);
+            vertical.setComponentAlignment(label, Alignment.TOP_LEFT);
         }
         nextMessageRightAlignment = !nextMessageRightAlignment;
     }
