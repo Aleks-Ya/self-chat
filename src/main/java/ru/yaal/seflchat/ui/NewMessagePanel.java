90e19d866a6ee20da256f28f9863b48d233d5b0f
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2016-06-27 19:56:39 +0300

    focus on new text message

--- src/main/java/ru/yaal/seflchat/ui/NewMessagePanel.java
+++ src/main/java/ru/yaal/seflchat/ui/NewMessagePanel.java
@@ -37,5 +37,6 @@ class NewMessagePanel extends VerticalLayout {
         addComponent(area);
         setComponentAlignment(area, Alignment.MIDDLE_CENTER);
         setSizeFull();
+        area.focus();
     }
 }
