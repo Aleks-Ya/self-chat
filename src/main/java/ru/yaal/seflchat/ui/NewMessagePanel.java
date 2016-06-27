9b0124f3af304d809cc0ababf5d1311d33482a8c
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2016-06-27 08:52:29 +0300

    add Ctrl-Enter listener

--- src/main/java/ru/yaal/seflchat/ui/NewMessagePanel.java
+++ src/main/java/ru/yaal/seflchat/ui/NewMessagePanel.java
@@ -1,5 +1,7 @@
 package ru.yaal.seflchat.ui;
 
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.event.ShortcutListener;
 import com.vaadin.ui.TextArea;
 import com.vaadin.ui.VerticalLayout;
 import ru.yaal.seflchat.data.Dialog;
@@ -16,15 +18,21 @@ public class NewMessagePanel extends VerticalLayout {
         area.setRows(5);
         area.setColumns(50);
 
-        area.addValueChangeListener(event -> {
-            String content = (String) event.getProperty().getValue();
-            if (!content.isEmpty()) {
-                MessageImpl message = new MessageImpl(content);
-                dialog.addMessage(message);
-                messagesPanel.addMessage(message);
-                area.clear();
+        ShortcutListener ctrlEnterListener = new ShortcutListener("Ctrl-Enter",
+                ShortcutAction.KeyCode.ENTER, new int[]{ShortcutAction.ModifierKey.CTRL}) {
+            @Override
+            public void handleAction(Object sender, Object target) {
+                String content = ((TextArea) target).getValue();
+                if (!content.isEmpty()) {
+                    MessageImpl message = new MessageImpl(content);
+                    dialog.addMessage(message);
+                    messagesPanel.addMessage(message);
+                    area.clear();
+                }
             }
-        });
+        };
+        area.addShortcutListener(ctrlEnterListener);
+
         addComponent(area);
         setSizeFull();
     }
