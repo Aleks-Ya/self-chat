4527cac950470e4287198c49ae994f5d0d82ea98
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2016-07-01 08:56:05 +0300

    fix run spring boot on wildfly

--- src/main/java/ru/yaal/seflchat/Application.java
+++ src/main/java/ru/yaal/seflchat/Application.java
@@ -2,12 +2,13 @@ package ru.yaal.seflchat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * @author Yablokov Aleksey
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
