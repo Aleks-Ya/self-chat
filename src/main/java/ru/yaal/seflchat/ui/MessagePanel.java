be1150e00774980cf64eb44a575f7d4c3aa9d3b8
Aleksey Yablokov <alexey.yablokov@trs-it.ru>
2016-07-03 13:36:11 +0300

    create MessagePanel

---
+++ src/main/java/ru/yaal/seflchat/ui/MessagePanel.java
@@ -0,0 +1,64 @@
+package ru.yaal.seflchat.ui;
+
+import com.vaadin.data.Property;
+import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.VerticalLayout;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import ru.yaal.seflchat.data.Message;
+
+/**
+ * @author Yablokov Aleksey
+ */
+@Component
+@Slf4j
+class MessagePanel extends Panel implements Property<Message> {
+    private final VerticalLayout vertical = new VerticalLayout();
+    private final Label label = new Label();
+    private Message value;
+
+    MessagePanel() {
+        log.info("Create " + getClass().getSimpleName());
+
+        label.setContentMode(ContentMode.HTML);
+        label.setReadOnly(true);
+        label.setWidth(70, Unit.PERCENTAGE);
+        vertical.addComponent(label);
+
+        setContent(vertical);
+        setSizeFull();
+    }
+
+    private void updateData() {
+        setText(value.getContent(), value.getAlignment());
+    }
+
+    private void setText(String text, Message.Alignment alignment) {
+        if (alignment == Message.Alignment.RIGHT) {
+            label.setValue("<p align='right'>" + text + "</p>");
+            vertical.setComponentAlignment(label, Alignment.TOP_RIGHT);
+        } else {
+            label.setValue("<p align='left'>" + text + "</p>");
+            vertical.setComponentAlignment(label, Alignment.TOP_LEFT);
+        }
+    }
+
+    @Override
+    public Message getValue() {
+        return value;
+    }
+
+    @Override
+    public void setValue(Message newValue) throws ReadOnlyException {
+        value = newValue;
+        updateData();
+    }
+
+    @Override
+    public Class<? extends Message> getType() {
+        return Message.class;
+    }
+}
--- src/main/java/ru/yaal/seflchat/ui/MessagesPanel.java
+++ src/main/java/ru/yaal/seflchat/ui/MessagesPanel.java
@@ -1,8 +1,5 @@
 package ru.yaal.seflchat.ui;
 
-import com.vaadin.shared.ui.label.ContentMode;
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
 import lombok.extern.slf4j.Slf4j;
@@ -25,7 +22,6 @@ class MessagesPanel extends Panel implements CurrentDialogService.DialogListener
     MessagesPanel(CurrentDialogService service) {
         this.service = service;
         log.info("Create " + getClass().getSimpleName());
-//        vertical.addComponentAttachListener(event -> markAsDirty());
         setScrollTop(9999);
         service.addListener(this);
         setContent(vertical);
@@ -33,18 +29,9 @@ class MessagesPanel extends Panel implements CurrentDialogService.DialogListener
     }
 
     private void addMessage(Message message) {
-        Label label = new Label();
-        label.setContentMode(ContentMode.HTML);
-        label.setReadOnly(true);
-        label.setWidth(70, Unit.PERCENTAGE);
-        vertical.addComponent(label);
-        if (message.getAlignment() == Message.Alignment.RIGHT) {
-            label.setValue("<p align='right'>" + message.getContent() + "</p>");
-            vertical.setComponentAlignment(label, Alignment.TOP_RIGHT);
-        } else {
-            label.setValue("<p align='left'>" + message.getContent() + "</p>");
-            vertical.setComponentAlignment(label, Alignment.TOP_LEFT);
-        }
+        MessagePanel panel = new MessagePanel();
+        panel.setValue(message);
+        vertical.addComponent(panel);
     }
 
     @Override
