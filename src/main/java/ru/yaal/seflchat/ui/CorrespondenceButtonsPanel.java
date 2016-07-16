74050462bd78b2d558773dd043d0c3c86ee07bca
Aleksey Yablokov <ya_al@bk.ru>
2016-07-16 08:39:06 +0300

    add dialog button event listener

--- src/main/java/ru/yaal/seflchat/ui/CorrespondenceButtonsPanel.java
+++ src/main/java/ru/yaal/seflchat/ui/CorrespondenceButtonsPanel.java
@@ -4,7 +4,10 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Panel;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import ru.yaal.seflchat.data.Dialog;
+import ru.yaal.seflchat.service.correspondence.CorrespondenceService;
 
 /**
  * @author Yablokov Aleksey
@@ -13,11 +16,12 @@ import org.springframework.stereotype.Component;
 @Component
 class CorrespondenceButtonsPanel extends Panel {
 
-    CorrespondenceButtonsPanel() {
+    @Autowired
+    CorrespondenceButtonsPanel(CorrespondenceService correspondenceService) {
         log.info("Create " + getClass().getSimpleName());
 
-        Button bAdd = new Button("Add correspondence");
-        Button bRemove = new Button("Remove correspondence");
+        Button bAdd = new Button("Add dialog", event -> correspondenceService.addDialog(new Dialog("new_dialog")));
+        Button bRemove = new Button("Remove dialog");
 
         HorizontalLayout layout = new HorizontalLayout();
         layout.addComponent(bAdd);
