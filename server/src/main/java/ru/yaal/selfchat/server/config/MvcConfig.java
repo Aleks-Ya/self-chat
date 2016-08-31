f27db52310fd6906ebf48c1bde18d3ff1cc557fa
Aleksei Iablokov <Aleksei_Iablokov@epam.com>
2016-08-31 12:12:14 +0300

    move embedded mongo dependency to vaadin-client

--- server/src/main/java/ru/yaal/selfchat/server/config/MvcConfig.java
+++ server/src/main/java/ru/yaal/selfchat/server/config/MvcConfig.java
@@ -1,9 +1,7 @@
 package ru.yaal.selfchat.server.config;
 
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
-@WebAppConfiguration
 public class MvcConfig {
 }
