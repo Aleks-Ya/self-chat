a97e31b3be5c09bf579c4ece8c9d83fed47c61e1
Aleksei Iablokov <Aleksei_Iablokov@epam.com>
2016-09-01 17:30:39 +0300

    remove Spring Boot from server

--- server/src/test/java/ru/yaal/selfchat/server/BaseSpringTest.java
+++ server/src/test/java/ru/yaal/selfchat/server/BaseSpringTest.java
@@ -1,10 +1,10 @@
 package ru.yaal.selfchat.server;
 
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = { TestConfig.class })
+@ContextConfiguration(classes = { TestConfig.class })
 public abstract class BaseSpringTest {
 }
