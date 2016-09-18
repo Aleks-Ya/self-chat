570c23d018be4d8221b865d22838004228695159
Aleksey Yablokov <ya_al@bk.ru>
2016-09-18 18:17:46 +0300

    Create *Repository interfaces

---
+++ server/src/main/java/ru/yaal/selfchat/server/repository/CorrespondenceRepository.java
@@ -0,0 +1,17 @@
+package ru.yaal.selfchat.server.repository;
+
+import java.util.List;
+
+import ru.yaal.selfchat.model.UserEntity;
+
+public interface CorrespondenceRepository {
+	UserEntity create(UserEntity user);
+
+	UserEntity get(int userId);
+
+	List<UserEntity> getAll();
+	
+	UserEntity update(UserEntity user);
+
+	boolean delete(int userId);
+}
