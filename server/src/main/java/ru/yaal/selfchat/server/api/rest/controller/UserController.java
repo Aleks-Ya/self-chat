60d988b25b1ea9ad2e14603a9322d322d0528bcc
Aleksey Yablokov <ya_al@bk.ru>
2016-09-18 22:37:45 +0300

    fix /user response content type

--- server/src/main/java/ru/yaal/selfchat/server/api/rest/controller/UserController.java
+++ server/src/main/java/ru/yaal/selfchat/server/api/rest/controller/UserController.java
@@ -16,6 +16,8 @@ import ru.yaal.selfchat.server.core.service.UserService;
 @RestController
 public class UserController {
 
+	private static final String JSON_MIME_TYPE = "application/json;charset=UTF-8";
+	
 	@Autowired
 	private UserService userService;
 
@@ -25,26 +27,26 @@ public class UserController {
 	/**
 	 * Test: curl -H "Content-Type: application/json" -X POST --data "{id: 1, login: aleks, passwordHash: abc}" localhost:8080/server-1.0/user
 	 */
-	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	@RequestMapping(value = "/user", method = RequestMethod.POST, produces = JSON_MIME_TYPE)
 	public String createUser(@RequestBody UserEntity user) {
 		final UserEntity newUser = userService.createUser(user);
 		return jsonService.userToJson(newUser);
 	}
 
-	@RequestMapping(value = "/user/{userId}", method = RequestMethod.GET)
+	@RequestMapping(value = "/user/{userId}", method = RequestMethod.GET, produces = JSON_MIME_TYPE)
 	public String getUser(@PathVariable int userId) {
 		final UserEntity user = userService.getUser(userId);
 		return jsonService.userToJson(user);
 	}
 
-	@RequestMapping(value = "/user/{userId}", method = RequestMethod.PUT)
+	@RequestMapping(value = "/user/{userId}", method = RequestMethod.PUT, produces = JSON_MIME_TYPE)
 	public String updateUser(@PathVariable int userId, @RequestBody UserEntity user) {
 		assert userId == user.getId();
 		userService.updateUser(user);
 		return "update " + userId;
 	}
 
-	@RequestMapping(value = "/user/{userId}", method = RequestMethod.DELETE)
+	@RequestMapping(value = "/user/{userId}", method = RequestMethod.DELETE, produces = JSON_MIME_TYPE)
 	public String deleteUser(@PathVariable int userId) {
 		return String.valueOf(userService.deleteUser(userId));
 	}
@@ -52,7 +54,7 @@ public class UserController {
 	/**
 	 * Test: curl localhost:8080/server-1.0/users
 	 */
-	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	@RequestMapping(value = "/users", method = RequestMethod.GET, produces = JSON_MIME_TYPE)
 	public String getAllUsers() {
 		final List<UserEntity> allUsers = userService.getAllUsers();
 		return jsonService.usersToJson(allUsers);
--- server/src/test/java/ru/yaal/selfchat/server/api/rest/controller/UserControllerTest.java
+++ server/src/test/java/ru/yaal/selfchat/server/api/rest/controller/UserControllerTest.java
@@ -1,10 +1,11 @@
 package ru.yaal.selfchat.server.api.rest.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import ru.yaal.selfchat.domian.security.UserEntity;
@@ -19,76 +20,78 @@ public class UserControllerTest extends BaseSpringTest {
 	public void createUserGenerateId() throws Exception {
 		UserEntity expUser = new UserEntity(null, "a", "hash");
 		String jsonUser = json.userToJson(expUser);
-		MockHttpServletRequestBuilder request = post("/user").contentType("application/json").content(jsonUser);
-		mockMvc.perform(request).andExpect(status().isOk());
+		MockHttpServletRequestBuilder request = post("/user").contentType(MediaType.APPLICATION_JSON).content(jsonUser);
+		mockMvc.perform(request).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(content().json(jsonUser));
 	}
 
 	// @Test
 	public void createUserWithId() {
-//		int id = 2;
-//		UserEntity expUser = new UserEntity(id, "с", "hash");
-//		String jsonUser = controller.createUser(expUser);
-//		UserEntity actUser = json.jsonToUser(jsonUser);
-//		assertThat(actUser.getId(), notNullValue());
-//		assertThat(actUser, equalTo(expUser));
+		// int id = 2;
+		// UserEntity expUser = new UserEntity(id, "с", "hash");
+		// String jsonUser = controller.createUser(expUser);
+		// UserEntity actUser = json.jsonToUser(jsonUser);
+		// assertThat(actUser.getId(), notNullValue());
+		// assertThat(actUser, equalTo(expUser));
 	}
 
 	// @Test
 	public void getUser() {
-//		int id = 3;
-//		UserEntity expUser = new UserEntity(id, "d", "hash");
-//		controller.createUser(expUser);
-//
-//		String jsonUser = controller.getUser(id);
-//		assertThat(jsonUser, notNullValue());
-//
-//		UserEntity actUser = json.jsonToUser(jsonUser);
-//		assertThat(actUser, equalTo(expUser));
+		// int id = 3;
+		// UserEntity expUser = new UserEntity(id, "d", "hash");
+		// controller.createUser(expUser);
+		//
+		// String jsonUser = controller.getUser(id);
+		// assertThat(jsonUser, notNullValue());
+		//
+		// UserEntity actUser = json.jsonToUser(jsonUser);
+		// assertThat(actUser, equalTo(expUser));
 	}
 
 	// @Test
 	public void updateUser() {
-//		int id = 4;
-//		UserEntity expUser = new UserEntity(id, "o", "hash");
-//		controller.createUser(expUser);
-//
-//		UserEntity actUser = json.jsonToUser(controller.getUser(id));
-//		assertThat(actUser, equalTo(expUser));
-//
-//		UserEntity expUserUpdated = expUser.withLogin("newLogin");
-//		controller.updateUser(id, expUserUpdated);
-//
-//		UserEntity actUserUpdated = json.jsonToUser(controller.getUser(id));
-//		assertThat(actUserUpdated, equalTo(expUserUpdated));
+		// int id = 4;
+		// UserEntity expUser = new UserEntity(id, "o", "hash");
+		// controller.createUser(expUser);
+		//
+		// UserEntity actUser = json.jsonToUser(controller.getUser(id));
+		// assertThat(actUser, equalTo(expUser));
+		//
+		// UserEntity expUserUpdated = expUser.withLogin("newLogin");
+		// controller.updateUser(id, expUserUpdated);
+		//
+		// UserEntity actUserUpdated = json.jsonToUser(controller.getUser(id));
+		// assertThat(actUserUpdated, equalTo(expUserUpdated));
 	}
 
 	// @Test
 	public void deleteUser() {
-//		int id = 5;
-//		UserEntity expUser = new UserEntity(id, "o", "hash");
-//		controller.createUser(expUser);
-//
-//		UserEntity actUser = json.jsonToUser(controller.getUser(id));
-//		assertThat(actUser, equalTo(expUser));
-//
-//		controller.deleteUser(id);
-//
-//		assertThat(controller.getUser(id), equalTo("null"));
+		// int id = 5;
+		// UserEntity expUser = new UserEntity(id, "o", "hash");
+		// controller.createUser(expUser);
+		//
+		// UserEntity actUser = json.jsonToUser(controller.getUser(id));
+		// assertThat(actUser, equalTo(expUser));
+		//
+		// controller.deleteUser(id);
+		//
+		// assertThat(controller.getUser(id), equalTo("null"));
 	}
 
 	// @Test
 	public void testGetAllUsers() {
-//		String login1 = "first";
-//		UserEntity expUser1 = new UserEntity(6, login1, "hash");
-//		controller.createUser(expUser1);
-//
-//		String login2 = "second";
-//		UserEntity expUser2 = new UserEntity(7, login2, "hash");
-//		controller.createUser(expUser2);
-//
-//		String allUsers = controller.getAllUsers();
-//		assertThat(allUsers, containsString(login1));
-//		assertThat(allUsers, containsString(login2));
+		// String login1 = "first";
+		// UserEntity expUser1 = new UserEntity(6, login1, "hash");
+		// controller.createUser(expUser1);
+		//
+		// String login2 = "second";
+		// UserEntity expUser2 = new UserEntity(7, login2, "hash");
+		// controller.createUser(expUser2);
+		//
+		// String allUsers = controller.getAllUsers();
+		// assertThat(allUsers, containsString(login1));
+		// assertThat(allUsers, containsString(login2));
 	}
 
 }
